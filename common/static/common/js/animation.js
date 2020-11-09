188ace2247c826e268effd049f0350f922564f19
Anastasia Zakharova <redwilliams26@yandex.ru>
2020-11-10 01:17:52 +0300

    commit

---
+++ common/static/common/js/animation.js
@@ -0,0 +1,12 @@
+var element = document.getElementById("make-order")
+
+function changeColor() 
+{
+    element.style.color = "#fff"
+    element.style.backgroundColor = "#A276FF"
+}
+
+function changeBack() {
+    element.style.color = "#A276FF"
+    element.style.backgroundColor = "none"
+}
