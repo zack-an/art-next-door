f24c268df34334b9375685893489b927611130e5
Anastasia Zakharova <redwilliams26@yandex.ru>
2020-11-11 14:55:00 +0300

    about us

--- common/models.py
+++ common/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Form(models.Model):
+    name = models.CharField('Name', max_length = 100)
+    email = models.EmailField('E-mail', max_length = 100)
+    tel = models.CharField('Phone', max_length = 20)
+    idea = models.TextField('Idea')
+
+    def __str__(self):
+        return self.name
--- common/views.py
+++ common/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from .models import Form
 
 class IndexView(View):
     def get(self, request):
         return render(request, 'common/index.html')
+
+'''def fill_form(request):'''
