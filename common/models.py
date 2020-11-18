1f106ca433923b451784b54a8cf41d0f0fcc18e5
Anastasia Zakharova <redwilliams26@yandex.ru>
2020-11-18 20:35:38 +0300

    sataaan

--- common/models.py
+++ common/models.py
@@ -5,6 +5,7 @@ class Form(models.Model):
     email = models.EmailField('E-mail', max_length = 100)
     phone = models.CharField('Phone', max_length = 20)
     idea = models.TextField('Idea')
+    #cover = models.FileField(upload_to='img/')
 
     def __str__(self):
         return self.name
